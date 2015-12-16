5df4da1a4d2fe3e5b429e9a351fdc695980de8a2
George Bearden <george@goerden.com>
2015-12-15 16:57:50 -0800

    OpenTK integration.

---
+++ SharpEarth.Wpf/WinFormsGLControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows.Forms;
+using System.Windows.Forms.Integration;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+namespace SharpEarth.Wpf
+{
+  public class WinFormsGlControl : WindowsFormsHost
+  {
+    GLControl glControl;
+    public WinFormsGlControl()
+    {
+      Initialized += OnInitialized;
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+      Initialized -= OnInitialized;
+
+      base.Dispose(disposing);
+    }
+
+    private void OnInitialized(object sender, System.EventArgs e)
+    {
+      var flags = GraphicsContextFlags.Default;
+      glControl = new GLControl(new GraphicsMode(32, 24), 2, 0, flags);
+      glControl.MakeCurrent();
+      glControl.Paint += OnPaint;
+      glControl.Dock = DockStyle.Fill;
+      Child = glControl;
+    }
+
+    private void OnPaint(object sender, PaintEventArgs paintEventArgs)
+    {
+      GL.ClearColor(Color4.Salmon);
+      GL.Clear(
+          ClearBufferMask.ColorBufferBit |
+          ClearBufferMask.DepthBufferBit |
+          ClearBufferMask.StencilBufferBit);
+
+      glControl.SwapBuffers();
+    }
+  }
+}
